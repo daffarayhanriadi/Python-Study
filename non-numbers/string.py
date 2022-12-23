4c87282530c6f532623c241f7cf5b32ef52a4b03
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-12-23 22:10:35 +0700

    Update string.py

--- non-numbers/string.py
+++ non-numbers/string.py
@@ -7,3 +7,18 @@ dan selanjutnya baris kedua'''
 print(s1)
 print()
 print(s2)
+
+
+# Slicing pada String
+s = "Hello World!"
+print('\n==Output Slicing String==')
+print(s[4])  # ambil karakter kelima dari string s
+print(s[6:11])  # ambil karakter ketujuh hingga sebelas dari string s
+# s[5] = "d"  # ubah karakter keenam dari string s menjadi "d", seharusnya gagal karena immutable
+print(s)
+
+# Ubah secara utuh String
+s = "Hello World!"
+s = "Halo Dunia!" # ubah isi string s menjadi "Halo Dunia!", seharusnya berhasil karena mutable
+print('\n==Output Update String==')
+print(s)
