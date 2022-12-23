7d05885c66fc0a66a2a614492036803aea87e158
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-12-23 22:10:19 +0700

    Create konversi.py

---
+++ Konversi-Tipe-Data/konversi.py
@@ -0,0 +1,11 @@
+print(float(5)) # int to float
+print(int(10.6)) # float to int
+print(int(-10.6))  # float to int (min)
+print(float('2.5')) # String to float
+print(str(25)) # int to String
+# print(int('1p')) # ERROR, karena tipe data yang tidak valid tidak dapat dikonversi
+print(set([1,2,3])) # list to set
+print(tuple({5,6,7})) # Set to tuple
+print(list('hello')) # String to list
+print(dict([[1,2],[3,4]])) # list to dict
+print(dict([(3,26),(4,44)])) # Tuple to dict
