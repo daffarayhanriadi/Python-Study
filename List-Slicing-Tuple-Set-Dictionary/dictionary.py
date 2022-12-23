e6b43178edad10487eb0c29b833bda9ae35a1846
Daffarr <77198942+Daffarr@users.noreply.github.com>
2022-12-23 22:10:23 +0700

    Create dictionary.py

---
+++ List-Slicing-Tuple-Set-Dictionary/dictionary.py
@@ -0,0 +1,11 @@
+d = {1: 'value', 'key': 2}
+print('\n==Output Dictionary==')
+print(type(d))
+print("d[1] = ", d[1])
+print("d['key'] = ", d['key'])
+
+
+d = {1: 'value', 'key': 2}
+
+# perintah berikut akan memberikan output error, karena dict. tidak bisa diakses secara index
+# print("d['2'] = ", d[2])
