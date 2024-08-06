1f98aeb0494eb77848c09b10ae362757737f53d5
Daffa Rayhan Riadi <77198942+daffarayhanriadi@users.noreply.github.com>
2024-08-06 22:53:57 +0700

    Update kt_standart_library.py

--- 12-Advanced Python Modules/kt_standart_library.py
+++ 12-Advanced Python Modules/kt_standart_library.py
@@ -18,3 +18,16 @@ import io
 
 file = io.open("12-Advanced Python Modules/file_text.txt", "r")
 print(file.read())
+
+"""
+Output:
+
+datetime now : 2024-08-06 22:53:19.013042
+tahun : 2024
+hari : Tuesday
+count data = Counter({'a': 3, 'd': 2, 'b': 1, 'c': 1})
+jumlah a = 3
+jumlah d = 2
+Say hello from Otong Surotong, makin sekut di mari
+
+"""
