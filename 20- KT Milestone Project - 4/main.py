3aa8f6309de961d3eab811ecf5bf86d5709fc9f5
Daffa Rayhan Riadi <77198942+daffarayhanriadi@users.noreply.github.com>
2024-08-07 22:39:31 +0700

    Create main.py

---
+++ 20- KT Milestone Project - 4/main.py
@@ -0,0 +1,44 @@
+import os
+import CRUD as CRUD
+
+if __name__ == "__main__":
+    sistem_operasi = os.name
+    
+    match sistem_operasi:
+        case "posix": os.system("clear")
+        case "nt": os.system("cls")
+    
+    print("SELAMAT DATANG DI PROGRAM")
+    print("DATABASE PERPUSTAKAAN")
+    print("=========================")
+    
+    # check database ada atau tidak
+    CRUD.init_console()
+    
+    while True:
+        match sistem_operasi:
+            case "posix": os.system("clear")
+            case "nt": os.system("cls")
+        
+        print("SELAMAT DATANG DI PROGRAM")
+        print("DATABASE PERPUSTAKAAN")
+        print("=========================")
+        
+        print(f"1. Read Data")
+        print(f"2. Create Data")
+        print(f"3. Update Data")
+        print(f"4. Delete Data")
+        
+        user_option = input("Masukkan opsi: ")
+        
+        match user_option:
+            case "1": CRUD.read_console()
+            case "2": CRUD.create_console()
+            case "3": print("Update Data")
+            case "4": print("Delete Data")
+            
+        is_done = input("Apakah Selesai (y/n)? ")
+        if is_done[0].lower() == "y":
+            break
+    
+    print("Program berakhir, Thanks!")
