6fdb75aeae4820a1deedaa79829bc65db0d6b88c
Daffa Rayhan Riadi <77198942+daffarayhanriadi@users.noreply.github.com>
2025-01-11 01:21:31 +0700

    Create colors.py

---
+++ 21-100 Day Code Challange Assignment/2-Day 15–31 - Intermediate python with TKinter/Day 18 - Intermediate - Turtle & the Graphical User Interface (GUI)/Project - Hirst Painting Project/colors.py
@@ -0,0 +1,51 @@
+# import os
+
+# import colorgram
+
+# # Use this code if ("image.png" or "./image.png", etc.) path is not work
+# current_dir = os.path.dirname(__file__)
+# image_path = os.path.join(current_dir, "image.png")
+
+# # Extract 30 colors from an image
+# colors = colorgram.extract(image_path, 30)
+# rgb_colors = []
+
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+color_list = [
+    (1, 9, 30),
+    (229, 235, 242),
+    (239, 232, 238),
+    (121, 95, 41),
+    (72, 32, 21),
+    (238, 212, 72),
+    (220, 81, 59),
+    (226, 117, 100),
+    (93, 1, 21),
+    (178, 140, 170),
+    (151, 92, 115),
+    (35, 90, 26),
+    (6, 154, 73),
+    (205, 63, 91),
+    (168, 129, 78),
+    (3, 78, 28),
+    (1, 64, 147),
+    (221, 179, 218),
+    (4, 220, 218),
+    (80, 135, 179),
+    (130, 157, 177),
+    (81, 110, 135),
+    (120, 187, 164),
+    (11, 213, 220),
+    (118, 18, 36),
+    (243, 205, 7),
+    (132, 223, 209),
+    (229, 173, 165),
+]
