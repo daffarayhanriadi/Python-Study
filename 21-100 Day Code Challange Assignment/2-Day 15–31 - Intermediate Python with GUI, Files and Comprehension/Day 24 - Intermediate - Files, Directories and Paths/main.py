b9004c2046c3ab8f616872842510a749a5edf573
Daffa Rayhan Riadi <77198942+daffarayhanriadi@users.noreply.github.com>
2025-01-19 18:08:19 +0700

    Update main.py

---
+++ 21-100 Day Code Challange Assignment/2-Day 15–31 - Intermediate Python with GUI, Files and Comprehension/Day 24 - Intermediate - Files, Directories and Paths/main.py
@@ -0,0 +1,12 @@
+with open("C:/Users/daffa/Desktop/my_file.txt", "r") as file:
+    contents = file.read()
+    print(contents)
+
+# if the current working directory is in C:/Users/daffa/PycharmProjects/WindowsDemo/main.py
+# with open("../../Desktop/my_file.txt", "r") as file:
+#     contents = file.read()
+#     print(contents)
+
+# Hello, my name is Daffa.
+# I am 12 years old.
+# My favourite food is a bowl of hot noodles.
