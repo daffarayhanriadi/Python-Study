33b729f0f8884f7f7f7c9c1943e4f113570f33a7
Daffa Rayhan Riadi <77198942+daffarayhanriadi@users.noreply.github.com>
2025-01-24 18:32:19 +0700

    Update my_code.py

--- 21-100 Day Code Challange Assignment/2-Day 15–31 - Intermediate Python with GUI, Files and Comprehension/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Project/my_code.py
+++ 21-100 Day Code Challange Assignment/2-Day 15–31 - Intermediate Python with GUI, Files and Comprehension/Day 25 - Intermediate - Working with CSV Data and the Pandas Library/Project/my_code.py
@@ -1,4 +1,5 @@
 import turtle as t
+
 import pandas as pd
 
 screen = t.Screen()
@@ -23,32 +24,29 @@ for state in df.state:
     all_states.append(state)
 
 while is_not_over:
-    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title() # type: ignore (just for hide the typechecking)
-
+    answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What's another state's name?").title()  # type: ignore (just for hide the typechecking)
 
     for state in df.state:
         if answer_state == state:
             a_state = df[df.state == answer_state]
-            writer.goto(a_state.x[a_state.index[0]], a_state.y[a_state.index[0]]) # type: ignore (just for hide the typechecking)
+            writer.goto(a_state.x[a_state.index[0]], a_state.y[a_state.index[0]])  # type: ignore (just for hide the typechecking)
             writer.write(answer_state)
             correct_answer.append(answer_state)
             score += 1
-    
+
     if answer_state == "Exit":
         for state in all_states:
             if state not in correct_answer:
                 all_missing_states.append(state)
-        missing_states = pd.DataFrame({
-            "State": all_missing_states
-        })
+        missing_states = pd.DataFrame({"State": all_missing_states})
         missing_states.to_csv("states_to_learn_2.csv")
         is_not_over = False
 
 # # To get the x & y coordinate
 # def get_mouse_click_coor(x, y):
 #     print(x, y)
-    
+
 # t.onscreenclick(get_mouse_click_coor)
 
 # t.mainloop()
-# screen.exitonclick()
+# screen.exitonclick()# screen.exitonclick()
